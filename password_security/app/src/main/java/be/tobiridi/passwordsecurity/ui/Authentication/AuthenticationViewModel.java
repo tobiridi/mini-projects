721981f274d779c7640ba4162158e23ffe070ba9
tobiridi <tony.6141@hotmail.com>
2025-01-10 11:20:06 +0100

    add authentication form

---
+++ password_security/app/src/main/java/be/tobiridi/passwordsecurity/ui/Authentication/AuthenticationViewModel.java
@@ -0,0 +1,8 @@
+package be.tobiridi.passwordsecurity.ui.Authentication;
+
+import androidx.lifecycle.ViewModel;
+
+public class AuthenticationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+
+}
