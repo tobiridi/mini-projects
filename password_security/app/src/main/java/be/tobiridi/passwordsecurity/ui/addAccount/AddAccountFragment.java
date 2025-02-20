4700b355f2a76d369efe7956b76973c7c0b8d2b5
tobiridi <tony.6141@hotmail.com>
2025-02-20 11:08:57 +0100

    improve snackbar above the bottom nav bar

--- password_security/app/src/main/java/be/tobiridi/passwordsecurity/ui/addAccount/AddAccountFragment.java
+++ password_security/app/src/main/java/be/tobiridi/passwordsecurity/ui/addAccount/AddAccountFragment.java
@@ -69,7 +69,7 @@ public class AddAccountFragment extends Fragment {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String msg;
+                String msg = "";
 
                 if (addAccountViewModel.createAccount(accountNameInputLayout, accountEmailInputLayout, accountPasswordInputLayout)) {
                     resetBtn.callOnClick();
@@ -86,6 +86,7 @@ public class AddAccountFragment extends Fragment {
 
                 Snackbar.make(v.getContext(), v, msg, Snackbar.LENGTH_SHORT)
                         .setAnimationMode(Snackbar.ANIMATION_MODE_FADE)
+                        .setAnchorView(R.id.bottomNavigationView)
                         .show();
             }
         };
